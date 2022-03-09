a3bf85de9b517f6b1b2f72268652078e6fcbae73
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2022-03-09 10:41:04 +0200

    update class 3 scripts; update gitignore

--- Class03-Raycasting/Assets/3D Raycasting/Scripts/ClickFromCamera3D.cs
+++ Class03-Raycasting/Assets/3D Raycasting/Scripts/ClickFromCamera3D.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ClickFromCamera3D : MonoBehaviour
 {
     public Camera mainCamera;
+    public LayerMask interactMask;
 
     void Update()
     {
@@ -23,16 +24,23 @@ public class ClickFromCamera3D : MonoBehaviour
 
         // Create a ray starting at click point on screen and moves along the camera perspective
         Ray clickRay = mainCamera.ScreenPointToRay(clickPosition);
-        
+
         // Declare a variable to store the Raycast hit information
         RaycastHit hit;
 
         // Physics.Raycast returns a bool (true/false) whether it hit a collider or not
-        if (Physics.Raycast(clickRay, out hit))
+        // Interaction mask allows us to filter what objects the ray should register
+        if (Physics.Raycast(clickRay, out hit, 100f, interactMask))
         {
             print(hit.transform.name);
-            
+
             // Do other logic with the object we clicked on
+            RandomiseColor random = hit.transform.GetComponent<RandomiseColor>();
+
+            if (random != null)
+            {
+                random.Randomise();
+            }
         }
         else
         {
@@ -44,6 +52,6 @@ public class ClickFromCamera3D : MonoBehaviour
         Vector3 clickOrigin = mainCamera.ScreenToWorldPoint(new Vector3(clickPosition.x, clickPosition.y, 0f));
 
         // Draw editor debugger to see ray in action
-        //Debug.DrawRay(clickOrigin, clickRay.direction * 100f, Color.yellow, 0.5f);
+        Debug.DrawRay(clickOrigin, clickRay.direction * 100f, Color.yellow, 0.5f);
     }
 }
