5d3c5907746e037c30171f5cdbddb4ddc5ac2df9
Ahmed ElShenawy <43609877+AShenawy@users.noreply.github.com>
2022-03-14 21:25:32 +0200

    update comment

--- Class05-UI_Interaction/Assets/Scripts/UIGoblin.cs
+++ Class05-UI_Interaction/Assets/Scripts/UIGoblin.cs
@@ -34,9 +34,10 @@ public class UIGoblin : MonoBehaviour, IPointerClickHandler, IDragHandler, IBegi
     }
 
     // OnDrop is called on this game object when we drop / end dragging on it with a mouse
-    // Important! This script is called not called on the object being dragged, but the object the drag is stopped on
-    // So if we dragged ObjA and dropped it on ObjB, OnEndDrag (if used) is called on ObjA & OnDrop (if used) is called on ObjB,
+    // Important!! This script is not called on the object being dragged, but the object the drag is ended on
+    // So if we dragged ObjA and _dropped_ it on ObjB, OnEndDrag (if used) is called on ObjA & OnDrop (if used) is called on ObjB,
     // this means that OnDrop should be placed not on the draggable object, but the object that the draggable will be dropped on.
+    // More info: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IDropHandler.html
     public void OnDrop(PointerEventData eventData)
     {
         print("Dropped something on me!");
