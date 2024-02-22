feef007ae5c508b285e2b39ecd958c673a21fcda
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-02-22 14:36:56 +0200

    Add comments

--- Class03-Raycasting/Assets/2D Raycasting/Scripts/Click2D/ObjectClick2D.cs
+++ Class03-Raycasting/Assets/2D Raycasting/Scripts/Click2D/ObjectClick2D.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class ObjectClick2D : MonoBehaviour
 {
+    // These will only work if the game object has a collider on it
+
     private void OnMouseDown()
     {
         print("Clicked on " + name);
