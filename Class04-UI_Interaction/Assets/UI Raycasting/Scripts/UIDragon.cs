207bfe2790dec7981f60ab0159169bd59337c166
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-03-01 10:28:35 +0200

    Update comments; Skip transparency on images

--- Class04-UI_Interaction/Assets/UI Raycasting/Scripts/UIDragon.cs
+++ Class04-UI_Interaction/Assets/UI Raycasting/Scripts/UIDragon.cs
@@ -1,10 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;  // This namespace is required to use the below interfaces
 
 public class UIDragon : MonoBehaviour, IDropHandler
 {
+    private void Awake()
+    {
+        // Prevent the transparent parts of the UI Image from triggering pointer events
+        // ## Important: For this to work, make sure Read/Write is enabled in the sprite's import settings in Unity
+        Image image = GetComponent<Image>();
+        image.alphaHitTestMinimumThreshold = 0.05f;
+    }
+
+
     // OnDrop is called on this game object when we drop / end dragging on it with a mouse
     // Important!! This script is not called on the object being dragged, but the object the drag is ended on
     // So if we dragged ObjA and _dropped_ it on ObjB, OnDrop (if used) is called first on ObjB, and OnEndDrag (if used) is called second on ObjA
--- Class04-UI_Interaction/Assets/UI Raycasting/Scripts/UIGoblin.cs
+++ Class04-UI_Interaction/Assets/UI Raycasting/Scripts/UIGoblin.cs
@@ -4,30 +4,65 @@ using UnityEngine;
 using UnityEngine.EventSystems; // This namespace is required to use the below interfaces
 using UnityEngine.UI;
 
-public class UIGoblin : MonoBehaviour, IPointerClickHandler, IDragHandler, IBeginDragHandler, IEndDragHandler
+public class UIGoblin : MonoBehaviour, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerClickHandler
 {
+    private void Awake()
+    {
+        // Prevent the transparent parts of the UI Image from triggering pointer events
+        // ## Important: For this to work, make sure Read/Write is enabled in the sprite's import settings in Unity
+        Image image = GetComponent<Image>();
+        image.alphaHitTestMinimumThreshold = 0.05f;
+    }
+
+
+    /* ## To be used with the Event Trigger component in Unity
+    public void TriggerClick(BaseEventData eventData)
+    {
+        print("Used trigger click");
+    }
+
+    public void TriggerBeginDrag(BaseEventData eventData)
+    {
+        if (eventData is PointerEventData pointerEvent)
+        {
+            print("starting to drag with trigger" + pointerEvent.pointerDrag);
+        }
+    }
+    */
+
+    // OnPointerClick is called only when we do a full click (button down and up) on the same object
+    // Docs ref: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IPointerClickHandler.html
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        print("clicked on " + name);
+    }
+
+
     // OnBeginDrag is called only once when we hold and start dragging the game object
     // If we don't drag, it won't get called
+    // Docs ref: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IBeginDragHandler.html
     public void OnBeginDrag(PointerEventData eventData)
     {
         // Store a reference to the object being dragged
-        GameObject draggedObject = eventData.pointerEnter;
+        GameObject draggedObject = eventData.pointerDrag;
         print("Begin dragging " + draggedObject.name);
 
-        // Disable the raycast target property to detect UI objects under the dragged element
+        // Disable the raycast target property to detect other UI objects under the dragged element
         GetComponent<Image>().raycastTarget = false;
     }
 
     // OnDrag is continuosuly called as long as we're dragging the game object
+    // Docs ref: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IDragHandler.html
     public void OnDrag(PointerEventData eventData)
     {
-        print("Dragging");
+        print($"Dragging {eventData.pointerDrag.name}...");
 
         // Increment the position value by the mouse delta (difference in mouse position from previous frame to current frame)
         transform.position += (Vector3)eventData.delta;
     }
 
     // OnEndDrag is called only once when we let go and stop dragging the game object
+    // Docs ref: https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IEndDragHandler.html
     public void OnEndDrag(PointerEventData eventData)
     {
         // Store a reference to the object being dragged
@@ -37,11 +72,5 @@ public class UIGoblin : MonoBehaviour, IPointerClickHandler, IDragHandler, IBegi
 
         // Enable the raycast target property to be able to detect drags and clicks on this object again
         GetComponent<Image>().raycastTarget = true;
-    }
-
-    // OnPointerClick is called when a full click occurs. I.E. mouse button is pressed and let go of while on top of the same game object
-    public void OnPointerClick(PointerEventData eventData)
-    {
-        print("Clicked!");
     }
 }
