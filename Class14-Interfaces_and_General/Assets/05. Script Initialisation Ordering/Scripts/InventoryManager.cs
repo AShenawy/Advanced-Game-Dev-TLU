cba961bb3b68cb800bbe35c82c703ee23332cd10
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2024-05-16 15:44:10 +0300

    Add script initialisation section

---
+++ Class14-Interfaces_and_General/Assets/05. Script Initialisation Ordering/Scripts/InventoryManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryManager : MonoBehaviour
+{
+    public static InventoryManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
