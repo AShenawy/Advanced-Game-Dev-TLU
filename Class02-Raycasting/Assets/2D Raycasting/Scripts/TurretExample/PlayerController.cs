164d42f7d901724a4588312522661369b8bd8b70
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2025-02-17 12:21:17 +0200

    Refactor PlayerController

--- Class02-Raycasting/Assets/2D Raycasting/Scripts/TurretExample/PlayerController.cs
+++ Class02-Raycasting/Assets/2D Raycasting/Scripts/TurretExample/PlayerController.cs
@@ -26,22 +26,7 @@ public class PlayerController : MonoBehaviour
     {
         // Get the main camera reference from the Camera class
         mainCamera = Camera.main;
-
-        // Calculate the half-width from the player's image boundaries along the horizontal x-axis. The bounds are the total width so we split them in 2
-        playerHalfWidth = playerImage.bounds.size.x * 0.5f;
-
-        // Get the right-most point on the game screen which is its width (as 'mainCamera.pixelWidth'),
-        // and project that point from the screen to the game world (using 'ScreenToWorldPoint' function from the main camera).
-        // This gives us that same point's coordinates, but inside the game world where game objects also live and move
-        rightScreenEdge = mainCamera.ScreenToWorldPoint(new Vector2(mainCamera.pixelWidth, 0)).x;
-
-        // Do the same for the left-most point on the game screen, which happens to be 0 on the screen
-        leftScreenEdge = mainCamera.ScreenToWorldPoint(Vector2.zero).x;
-
-		// Calculate the maximum and minimum possible X values for the players position along the X-axis
-		// Taking into account the player's own width so it stays completely on screen
-        maxPosX = rightScreenEdge - playerHalfWidth;
-        minPosX = leftScreenEdge + playerHalfWidth;
+        SetupScreenEdges();
     }
 
     // Update is called once per frame
@@ -71,5 +56,24 @@ public class PlayerController : MonoBehaviour
 
             gameObject.transform.position = Vector2.MoveTowards(currentPos, newPos, moveSpeed * Time.deltaTime);
         }
+    }
+
+    void SetupScreenEdges()
+    {
+        // Calculate the half-width from the player's image boundaries along the horizontal x-axis. The bounds are the total width so we split them in 2
+        playerHalfWidth = playerImage.bounds.size.x * 0.5f;
+
+        // Get the right-most point on the game screen which is its width (as 'mainCamera.pixelWidth'),
+        // and project that point from the screen to the game world (using 'ScreenToWorldPoint' function from the main camera).
+        // This gives us that same point's coordinates, but inside the game world where game objects also live and move
+        rightScreenEdge = mainCamera.ScreenToWorldPoint(new Vector2(mainCamera.pixelWidth, 0)).x;
+
+        // Do the same for the left-most point on the game screen, which happens to be 0 on the screen
+        leftScreenEdge = mainCamera.ScreenToWorldPoint(Vector2.zero).x;
+
+        // Calculate the maximum and minimum possible X values for the players position along the X-axis
+        // Taking into account the player's own width so it stays completely on screen
+        maxPosX = rightScreenEdge - playerHalfWidth;
+        minPosX = leftScreenEdge + playerHalfWidth;
     }
 }
