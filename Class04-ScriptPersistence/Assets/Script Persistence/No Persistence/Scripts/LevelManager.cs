da67988aedc2eb459a9e37a263872a8ba869318d
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2025-03-03 11:12:33 +0200

    Add console messages for non-persistent example

--- Class04-ScriptPersistence/Assets/Script Persistence/No Persistence/Scripts/LevelManager.cs
+++ Class04-ScriptPersistence/Assets/Script Persistence/No Persistence/Scripts/LevelManager.cs
@@ -13,13 +13,17 @@ namespace NonPersisting
         // Unity button action
         public void LoadLevel(string levelName)
         {
+            print("Loading new level: " + levelName);
+
             // store current scene name as previous level before loading the new scene
             previousLevel = SceneManager.GetActiveScene().name;
+            print("Previously loaded level was: " + previousLevel);
 
             SceneManager.LoadScene(levelName);
 
             // store newly loaded scene name as the current level
             currentLevel = levelName;
+            print($"New level: {levelName} is loaded");
         }
     }
 }
