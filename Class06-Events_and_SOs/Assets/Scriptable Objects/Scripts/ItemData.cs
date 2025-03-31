ee1a49009d2d6be5cb141a52472c4e36b6a9e50e
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2025-03-31 22:37:58 +0300

    Add WeaponData SO & Enum

--- Class06-Events_and_SOs/Assets/Scriptable Objects/Scripts/ItemData.cs
+++ Class06-Events_and_SOs/Assets/Scriptable Objects/Scripts/ItemData.cs
@@ -12,3 +12,42 @@ public class ItemData : ScriptableObject
     [Tooltip("The max quantity the player can carry of this item")]
     public int maxAmount;
 }
+
+public enum Grade
+{
+    Normal,
+    Rare,
+    Epic
+}
+
+[CreateAssetMenu(fileName = "New Weapon", menuName = "Game/Weapon Data")]
+public class WeaponData : ScriptableObject
+{
+    public string itemName = "The best weapon";
+    [SerializeField] Sprite spriteNormal;
+    [SerializeField] Sprite spriteRare;
+    [SerializeField] Sprite spriteEpic;
+
+    [Tooltip("How many times the item can be used before it's depleted")]
+    public int usageCount;
+
+    [Tooltip("The max quantity the player can carry of this item")]
+    public int maxAmount;
+
+
+    public Sprite GetSprite(Grade grade)
+    {
+        switch (grade)
+        {
+            case Grade.Normal:
+                return spriteNormal;
+            case Grade.Rare:
+                return spriteRare;
+            case Grade.Epic:
+                return spriteEpic;
+            default:
+                Debug.LogWarning("Undefined grade");
+                return spriteNormal;
+        }
+    }
+}
