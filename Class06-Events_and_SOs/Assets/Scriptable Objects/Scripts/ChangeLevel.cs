416cc72f45548a57afd6bd37194a9e5f0bfca320
Ahmed ElShenawy <elshenawy.ahmed@gmail.com>
2025-03-31 15:56:45 +0300

    Add namespace

--- Class06-Events_and_SOs/Assets/Scriptable Objects/Scripts/ChangeLevel.cs
+++ Class06-Events_and_SOs/Assets/Scriptable Objects/Scripts/ChangeLevel.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ChangeLevel : MonoBehaviour
 {
     public void Change(string levelName)
     {
-        UnityEngine.SceneManagement.SceneManager.LoadScene(levelName);
+        SceneManager.LoadScene(levelName);
     }
 }
